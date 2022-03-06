d183958102c4d7786485d25a1d609a382d1d02b5
imnotmoon <imnotmoon@gmail.com>
2022-03-06 22:22:50 +0900

    Close when clicked outside of modal

--- todo-storybook/src/_components/Modal/Modal.tsx
+++ todo-storybook/src/_components/Modal/Modal.tsx
@@ -1,4 +1,4 @@
-import React, { useContext, useEffect } from 'react';
+import React, { useContext, useEffect, useRef } from 'react';
 import './Modal.style.scss';
 import { Formik, Form } from 'formik';
 
@@ -15,14 +15,22 @@ type ModalValue = TodoType;
 
 const Modal: React.FC<ModalProps> = ({ close }) => {
     const { todoList, setTodoList } = useContext(LocalTodoListContext);
+    const modalBackgroundEl = useRef<HTMLDivElement>(null);
+    const modalFrameEl = useRef<HTMLDivElement>(null);
 
     useEffect(() => {
-        const onClickOutsideModal = () => {
-            close();
+        const onClickOutsideModal = (e: MouseEvent) => {
+            const isClicked = modalBackgroundEl.current?.contains(e.target as Node);
+            const isOutsideClicked = !modalFrameEl.current?.contains(e.target as Node);
+            if(isClicked && isOutsideClicked) {
+                close();
+            }
         }
-        document.querySelector('body')!.addEventListener('click', onClickOutsideModal);
-        return document.querySelector('body')!.removeEventListener('click', onClickOutsideModal);
-    }, [])
+        document.body.addEventListener('click', onClickOutsideModal);
+        return () => {
+            document.body.removeEventListener('click', onClickOutsideModal);
+        };
+    }, []);
 
     const onClickSubmit = (values: Omit<ModalValue, 'timestamp'> & { due: Date | string }) => {
         setTodoList([...todoList, {
@@ -34,7 +42,7 @@ const Modal: React.FC<ModalProps> = ({ close }) => {
     }
 
     return (
-      <div className='modal'>
+      <div className='modal' ref={modalBackgroundEl}>
           <Formik
               initialValues={{
                   title: '',
@@ -46,7 +54,7 @@ const Modal: React.FC<ModalProps> = ({ close }) => {
               onSubmit={onClickSubmit}
           >
               <Form className='modal-form'>
-                  <div className='modal-form-window'>
+                  <div className='modal-form-window' ref={modalFrameEl}>
                       <h3>할일 추가하기</h3>
                       <ModalForm />
                       <section className='modal-window-buttons'>
